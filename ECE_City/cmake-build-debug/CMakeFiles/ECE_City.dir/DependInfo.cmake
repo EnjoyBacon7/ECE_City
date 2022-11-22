5ec58a3ff9fcc86b5cd852a666133f6b1aa1ebb5
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2022-11-22 14:48:13 +0100

    things

---
+++ ECE_City/cmake-build-debug/CMakeFiles/ECE_City.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/camille/Documents/GitHub/ECE_City/ECE_City/affichage.c" "CMakeFiles/ECE_City.dir/affichage.c.o" "gcc" "CMakeFiles/ECE_City.dir/affichage.c.o.d"
+  "/Users/camille/Documents/GitHub/ECE_City/ECE_City/boucleJeu.c" "CMakeFiles/ECE_City.dir/boucleJeu.c.o" "gcc" "CMakeFiles/ECE_City.dir/boucleJeu.c.o.d"
+  "/Users/camille/Documents/GitHub/ECE_City/ECE_City/extras.c" "CMakeFiles/ECE_City.dir/extras.c.o" "gcc" "CMakeFiles/ECE_City.dir/extras.c.o.d"
+  "/Users/camille/Documents/GitHub/ECE_City/ECE_City/initPartie.c" "CMakeFiles/ECE_City.dir/initPartie.c.o" "gcc" "CMakeFiles/ECE_City.dir/initPartie.c.o.d"
+  "/Users/camille/Documents/GitHub/ECE_City/ECE_City/main.c" "CMakeFiles/ECE_City.dir/main.c.o" "gcc" "CMakeFiles/ECE_City.dir/main.c.o.d"
+  "/Users/camille/Documents/GitHub/ECE_City/ECE_City/menus.c" "CMakeFiles/ECE_City.dir/menus.c.o" "gcc" "CMakeFiles/ECE_City.dir/menus.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ECE_City/cmake-build-debug/CMakeFiles/ECE_City.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ECE_City.dir/affichage.c.o"
+  "CMakeFiles/ECE_City.dir/affichage.c.o.d"
+  "CMakeFiles/ECE_City.dir/boucleJeu.c.o"
+  "CMakeFiles/ECE_City.dir/boucleJeu.c.o.d"
+  "CMakeFiles/ECE_City.dir/extras.c.o"
+  "CMakeFiles/ECE_City.dir/extras.c.o.d"
+  "CMakeFiles/ECE_City.dir/initPartie.c.o"
+  "CMakeFiles/ECE_City.dir/initPartie.c.o.d"
+  "CMakeFiles/ECE_City.dir/main.c.o"
+  "CMakeFiles/ECE_City.dir/main.c.o.d"
+  "CMakeFiles/ECE_City.dir/menus.c.o"
+  "CMakeFiles/ECE_City.dir/menus.c.o.d"
+  "ECE_City"
+  "ECE_City.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ECE_City.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
